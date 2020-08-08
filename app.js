062db91ea435d0583cd4adefe425dcc169687ab1
James <jamescarclan@yahoo.com>
2020-08-08 17:07:10 +0200

    iteration 1/2/3

--- app.js
+++ app.js
@@ -22,4 +22,16 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi[0]);
+      res.render('beers', {
+        allBeers: beersFromApi
+      });
+    })
+    .catch(erreur => console.log(erreur));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
