23893bcc170a2a7ec7f4abe6dcb7be0f4947d466
toufiq23 <drrameraaron023@gmail.com>
2020-08-08 17:29:47 +0200

    70% done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -22,4 +23,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+	punkAPI
+	.getBeers()
+	.then(beersFromApi => {
+		res.render('beers', {beersFromApi});
+		console.log(beersFromApi);
+	})
+	.catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+	punkAPI
+	.getRandom()
+	.then(responseFromAPI => {
+		res.render('random-beer', {responseFromAPI});
+	})
+	.catch(error => console.log(error));
+});
+
+app.listen(3010, () => console.log('🏃‍ on port 3000'));
