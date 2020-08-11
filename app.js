68492d2bb9494a5bc1eeeb4824193be1efc53839
Nina Quaresma <quaresma.nina@gmail.com>
2020-08-11 20:43:28 +0200

    Iteration 4 + 5 + début style

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,6 +34,17 @@ app.get('/beers', (req, res) => {
       });
     })
     .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(function (beer) {
+      res.render('random-beer', {
+        beer: beer
+      });
+    })
+    .catch(error => console.log(error));
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
