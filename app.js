fcaade1a4f366ce3d865412d91c56ee6f373ca7c
Antoine BERNIER <antoine.bernier@gmail.com>
2020-09-01 18:59:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+// 1. creation app express
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// 2. configuration de l'app
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+// 3. middlewares
 app.use(express.static(`${__dirname}/public`));
 
 // Register the location for handlebars partials here:
 hbs.registerPartials(__dirname + '/views/partials');
 
-// Add the route handlers here:
+//
+// 4. Add the route handlers here:
+//
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -45,4 +50,5 @@ app.get('/random-beer', (req, res, next) => {
   })
 });
 
+// 5. start to listen requests
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
